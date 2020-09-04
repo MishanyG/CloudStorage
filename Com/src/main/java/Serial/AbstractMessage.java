984fa0708cbd759cf40009926af4f7864c00886b
Misha Glushak <flatron_777@mail.ru>
2020-09-04 22:44:18 +1000

    Домашняя работа №4 (В стадии разработки)

--- Com/src/main/java/Serial/AbstractMessage.java
+++
@@ -1,6 +0,0 @@
-package Serial;
-
-import java.io.Serializable;
-
-public class AbstractMessage implements Serializable {
-}
--- Com/src/main/java/Serial/Step.java
+++
@@ -1,9 +0,0 @@
-package Serial;
-
-import java.io.Serializable;
-
-public enum Step implements Serializable {
-    UPLOAD,
-    DOWNLOAD,
-    DIR
-}
